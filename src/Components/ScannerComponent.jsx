e0707333089ad882b9bf5bdffa572210b3511ab2
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-20 11:02:49 +0300

    fix for scanner only in 1 object num 8

--- src/Components/ScannerComponent.jsx
+++ src/Components/ScannerComponent.jsx
@@ -3,65 +3,48 @@ import { Html5Qrcode } from "html5-qrcode";
 
 function ScannerComponent({ scannedDataFromScanner }) {
   const [scannedDataInScanner, setScannedDataInScanner] = useState("");
-  const [isScannerOpen, setIsScannerOpen] = useState(false);
 
   const handleQrCodeSuccess = (decodedText, decodedResult) => {
     setScannedDataInScanner(decodedText);
     scannedDataFromScanner(decodedText); // pass the scanned data back to the SampleBag component
     Html5Qrcode.stop(); // stop scanning
-    setIsScannerOpen(false);
   };
 
-  const openScanner = () => {
-    setIsScannerOpen(true);
-    Html5Qrcode.getCameras()
-      .then((devices) => {
-        if (devices && devices.length) {
-          const html5QrCode = new Html5Qrcode("reader");
-          const config = {
-            fps: 100,
-            qrbox: {
-              width: window.screen.width < 600 ? 200 : 300,
-              height: window.screen.width < 600 ? 100 : 100,
-            },
-            aspectRatio: 1,
-          };
+  Html5Qrcode.getCameras()
+    .then((devices) => {
+      if (devices && devices.length) {
+        const html5QrCode = new Html5Qrcode("reader");
+        const config = {
+          fps: 100,
+          qrbox: {
+            width: window.screen.width < 600 ? 200 : 300,
+            height: window.screen.width < 600 ? 100 : 100,
+          },
+          aspectRatio: 1,
+        };
 
-          try {
-            html5QrCode.start(
-              { facingMode: { exact: "environment" } },
-              config,
-              handleQrCodeSuccess
-            );
-            setTimeout(function () {
-              html5QrCode.applyVideoConstraints({
-                focusMode: "continuous",
-                advanced: [{ zoom: 2.0 }],
-              });
-            }, 2000);
-          } catch (error) {
-            console.log("Unable to start scanning.", error);
-          }
+        try {
+          html5QrCode.start(
+            { facingMode: { exact: "environment" } },
+            config,
+            handleQrCodeSuccess
+          );
+          setTimeout(function () {
+            html5QrCode.applyVideoConstraints({
+              focusMode: "continuous",
+              advanced: [{ zoom: 2.0 }],
+            });
+          }, 2000);
+        } catch (error) {
+          console.log("Unable to start scanning.", error);
         }
-      })
-      .catch((err) => {
-        console.log("Error getting cameras", err);
-      });
-  };
+      }
+    })
+    .catch((err) => {
+      console.log("Error getting cameras", err);
+    });
 
-  return (
-    <div>
-      <button
-        type="button"
-        className="btn btn-secondary"
-        onClick={openScanner}
-        disabled={isScannerOpen}
-      >
-        Scan Barcode
-      </button>
-      {isScannerOpen && <div id="reader"></div>}
-    </div>
-  );
+  return <div id="reader"></div>;
 }
 
 export default ScannerComponent;
